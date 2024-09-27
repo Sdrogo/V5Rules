b395787367ebb35751fba1bcd3c3e82e270cef08
Sdrogo <andreapartenope@gmail.com>
2024-09-28 00:31:37 +0200

    - General Theme refactor - Navigation animation refactor - code clean-up

--- app/src/main/java/com/example/v5rules/CustomNavHost.kt
+++ app/src/main/java/com/example/v5rules/CustomNavHost.kt
@@ -5,10 +5,6 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
