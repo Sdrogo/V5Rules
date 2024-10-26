8eab486eac91ebb2a2d62d7f56689ae82042101c
Sdrogo <andreapartenope@gmail.com>
2024-10-26 14:55:44 +0200

    - fixing Npc Generator nationality list

--- app/src/main/java/com/example/v5rules/data/Chapter.kt
+++ app/src/main/java/com/example/v5rules/data/Chapter.kt
@@ -3,11 +3,13 @@ package com.example.v5rules.data
 data class Chapter(
     val title: String,
     val content: String = "",
+    val table: Table? = null,
     val sections: List<Paragraph>? = null
 )
 
 data class Paragraph(
     val title: String,
     val content: String,
+    val table: Table? = null,
     val subParagraphs: List<Paragraph>? = null,
 )
